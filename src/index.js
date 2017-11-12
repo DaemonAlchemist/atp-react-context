51ca5a36e51a79cff0e84ebdac9ec4ce35d0a7bb
Andrea <andrea@wittrock.us>
2017-11-11 22:06:40 -0600

    Initial implementation

--- src/index.js
+++ src/index.js
@@ -2,3 +2,24 @@
  * Created by Andy on 7/9/2017.
  */
 
+import React from "react";
+import {o} from 'atp-sugar';
+    ''
+const provideContext = contextTypes => Component => class ContextProvider extends React.Component {
+    static childContextTypes = contextTypes;
+    getChildContext() {
+        return o(contextTypes).map((_, key) => this.props[key]);
+    }
+    render() {
+        return <Component {...this.props} />
+    }
+};
+
+const addContext = contextTypes => Component => class ContextConsumer extends React.Component {
+    static contextTypes = contextTypes;
+    render() {
+        return <Component {...this.props} {...this.context} />
+    }
+};
+
+export {provideContext, addContext};
